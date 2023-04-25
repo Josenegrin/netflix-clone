11581d1160dfe8bbc8900696438faf4cdd932c88
Jose Negrin <soyjosenegrin@gmail.com>
2023-04-25 20:27:05 +0200

    added input component

---
+++ src/components/Input/input.tsx
@@ -0,0 +1,34 @@
+import React, { HTMLInputTypeAttribute } from "react";
+
+interface InputProps {
+  id: string;
+  placeholder: string;
+  onChange: any;
+  value: string;
+  label: string;
+  type?: HTMLInputTypeAttribute 
+}
+
+const Input: React.FC<InputProps> = ({
+  id, onChange, value, label, type
+}) => {
+  return (
+    <div className="relative">
+      <input
+      placeholder=' '
+      value={value}
+      onChange={onChange}
+      type={type}
+      className='block rounded-md py-6 px-6 text-md appearance-none focus:outline-none focus:ring-0 bg-neutral-700 text-white peer self-center w-full'
+      id={id}
+      />
+      <label
+      htmlFor={id}
+      className="absolute text-md text-zinc-400 duration-150 transform -translate-y-3 scale-75 top-4 z-10 origin-[0] left-6 peer-placeholder-shown:scale-100 peer-placeholder-shown:translate-y-0 peer-focus:scale-75 peer-focus:-translate-y-3">
+        {label}
+      </label>
+    </div>
+  )
+}
+
+export default Input
