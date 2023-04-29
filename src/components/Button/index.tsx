80d87bf3435877dcc5856eeba8cccb8d05b6fcbe
Jose Negrin <soyjosenegrin@gmail.com>
2023-04-30 01:05:29 +0200

    added onclick event in the button component

--- src/components/Button/index.tsx
+++ src/components/Button/index.tsx
@@ -2,11 +2,15 @@ import React from 'react'
 
 interface ButtonProps {
   text?: string
+  onClick: React.MouseEventHandler<HTMLButtonElement>;
 }
 
-const Button: React.FC<ButtonProps> = ({text}) => {
+const Button: React.FC<ButtonProps> = ({text, onClick}) => {
   return (
-    <button className='bg-red-600 text-white rounded-md  w-full py-3 mt-10 hover:bg-red-700 transition'>
+    <button
+      onClick={onClick}
+      className='bg-red-600 text-white rounded-md  w-full py-3 mt-10 hover:bg-red-700 transition'
+    >
       {text}
     </button>
   )
