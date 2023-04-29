e9d4c05bf2304ff50c55910f5a1931a716bf911a
Jose Negrin <soyjosenegrin@gmail.com>
2023-04-29 21:45:41 +0200

    added spanish translations

---
+++ src/locale/es.ts
@@ -0,0 +1,32 @@
+export const es = {
+  auth: {
+    login: {
+      title: 'Iniciar sesión',
+      button: 'Iniciar sesión',
+      footer: {
+        text: '¿Todavía sin Netflix?',
+        action: 'Suscríbete ya',
+      }
+    },
+    register: {
+      title: 'Crea una cuenta',
+      button: 'Crea una cuenta',
+      footer: {
+        text: '¿Ya tienes una cuenta?',
+        action: 'Inicia sesión',
+      }
+    },
+    form: {
+      email: {
+        label: 'Escribe tu correo electrónico'
+      },
+      username: {
+        label: 'Escribe tu nombre de usuario'
+      },
+      password: {
+        label: 'Escribe tu contraseña'
+      },
+    }
+  }
+
+}
