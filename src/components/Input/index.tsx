182eeb1dd878bd1b0fc2ce3c29a9394823b193f7
Jose Negrin <soyjosenegrin@gmail.com>
2023-04-29 21:44:56 +0200

    modified input component

--- src/components/Input/index.tsx
+++ src/components/Input/index.tsx
@@ -1,8 +1,7 @@
 import React, { HTMLInputTypeAttribute } from "react";
 
 interface InputProps {
-  id: string;
-  placeholder: string;
+  name: string;
   onChange: any;
   value: string;
   label: string;
@@ -10,7 +9,7 @@ interface InputProps {
 }
 
 const Input: React.FC<InputProps> = ({
-  id, onChange, value, label, type
+  name, onChange, value, label, type
 }) => {
   return (
     <div className="relative">
@@ -20,10 +19,10 @@ const Input: React.FC<InputProps> = ({
       onChange={onChange}
       type={type}
       className='block rounded-md py-6 px-6 text-md appearance-none focus:outline-none focus:ring-0 bg-neutral-700 text-white peer self-center w-full'
-      id={id}
+      name={name}
       />
       <label
-      htmlFor={id}
+      htmlFor={name}
       className="absolute text-md text-zinc-400 duration-150 transform -translate-y-3 scale-75 top-4 z-10 origin-[0] left-6 peer-placeholder-shown:scale-100 peer-placeholder-shown:translate-y-0 peer-focus:scale-75 peer-focus:-translate-y-3">
         {label}
       </label>
