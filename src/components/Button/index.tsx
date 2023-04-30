b441cd76774c1e196a69fd4da43f0c02a6e67a48
Jose Negrin <soyjosenegrin@gmail.com>
2023-04-30 21:30:43 +0200

    modified the button component to set a custom style and default styles

--- src/components/Button/index.tsx
+++ src/components/Button/index.tsx
@@ -1,17 +1,21 @@
 import React from 'react'
 
 interface ButtonProps {
+  children: React.ReactElement<HTMLParagraphElement> | React.ReactNode;
   text?: string
+  styles?: string
   onClick: React.MouseEventHandler<HTMLButtonElement>;
 }
 
-const Button: React.FC<ButtonProps> = ({text, onClick}) => {
+const Button: React.FC<ButtonProps> = ({children, styles, onClick}) => {
+const defaultClassName = 'bg-red-600 text-white rounded-md  w-full py-3 mt-10 hover:bg-red-700 transition'
+
   return (
     <button
       onClick={onClick}
-      className='bg-red-600 text-white rounded-md  w-full py-3 mt-10 hover:bg-red-700 transition'
+      className={ styles ? styles : defaultClassName}
     >
-      {text}
+      {children}
     </button>
   )
 }
