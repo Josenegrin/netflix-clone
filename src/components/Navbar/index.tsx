0fd6b351e67a4451af0c57d77f7b35152c346e79
Jose Negrin <soyjosenegrin@gmail.com>
2023-05-01 20:50:39 +0200

    added navbar and menu items options

---
+++ src/components/Navbar/index.tsx
@@ -0,0 +1,67 @@
+import React, { useState, useEffect } from 'react'
+import ResponsiveImage from '../ResponsiveImage/indes'
+import NavbarItem from '../NavbarItem/indes'
+import { BsSearch, BsBell } from 'react-icons/bs'
+import MobileMenu from '../MobileMenu'
+import { menuItems } from '@/lib/menuItems'
+import AccountMenu from '../AccountMenu'
+
+const TOP_OFFSET = 66
+
+const Navbar = () => {
+  const [showBackground, setShowBackground] = useState<boolean>(false)
+
+  useEffect(() => {
+    const handleScroll = () => {
+      if (window.scrollY >=  TOP_OFFSET) {
+        setShowBackground(true)
+      } else {
+        setShowBackground(false)
+      }
+    }
+
+    window.addEventListener('scroll', handleScroll)
+
+    return () => {
+      window.removeEventListener('scroll', handleScroll)
+    }
+  }, [])
+
+  return (
+    <nav className='w-full fixed z-40'>
+      <div
+        className={`
+          px-4
+          md:px-16
+          py-6
+          flex
+          flex-row
+          items-center
+          transition
+          duration-500
+          ${showBackground ? 'bg-zinc-900 bg-opacity-90' : ''}`}
+      >
+        <ResponsiveImage
+          styles='h-4 w-20 md:h-7 md:w-36'
+          src='/images/logo.png'
+          alt='Logo'
+        />
+        <div className='flex-row ml-8 gap-7 hidden lg:flex'>
+          {menuItems.map((item) => <NavbarItem key={`NavItem-${item.label}`} label={item.label} />)}
+        </div>
+          <MobileMenu />
+          <div className='flex flex-row ml-auto gap-7 items-center'>
+            <div className='text-gray-200 hover:text-gray-300 cursor-pointer transition'>
+              <BsSearch />
+            </div>
+            <div className='text-gray-200 hover:text-gray-300 cursor-pointer transition'>
+              <BsBell />
+            </div>
+            <AccountMenu/>
+        </div>
+      </div>
+    </nav>
+  )
+}
+
+export default Navbar
---
+++ src/lib/menuItems.ts
@@ -0,0 +1,8 @@
+export const menuItems = [
+  {label: 'Home', route: ''},
+  {label: 'Series', route: ''},
+  {label: 'Films', route: ''},
+  {label: 'New & Popular', route: ''},
+  {label: 'Browse by languages', route: ''},
+  {label: 'My List', route: ''}
+]
