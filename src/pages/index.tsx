bdd440d66c84eabb5227060e8c12e47b72c1470a
Jose Negrin <soyjosenegrin@gmail.com>
2023-06-20 21:14:04 +0200

    imported billboard section to the index

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,9 +1,31 @@
+import { NextPageContext } from 'next'
+import { getSession } from 'next-auth/react'
+import Billboard from '@/components/Billboard'
 import Navbar from '@/components/Navbar'
 
 export default function Home() {
+
   return (
-  <>
+  <div>
     <Navbar />
-  </>
+    <Billboard />
+  </div>
   )
+}
+
+export async function getServerSideProps(context: NextPageContext) {
+  const session = await getSession(context)
+
+  if (!session) {
+    return {
+      redirect: {
+        destination: '/auth',
+        permanent: false,
+      }
+    }
+  }
+
+  return {
+    props: {}
+  }
 }
